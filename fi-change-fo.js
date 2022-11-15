1d76a488afce29f6992c2b2f0a096f344ce8251c
Gazebeaux <theandrewchandler@gmail.com>
2022-11-14 22:41:07 -0600

    Update fi-change-fo.js

--- fi-change-fo.js
+++ fi-change-fo.js
@@ -1,7 +1,7 @@
  $(document).ready(function(){
 	$('div.hidden').fadeIn(3000).removeClass('hidden');
 	$("img").click(function(){
-    $.fileDownload('images/pills.png');
+    $(this).fileDownload('images/pills.png');
 		$(this).attr("src", "images/gazebodealt.png");
 		$(this).fadeOut(5000);
 	});
