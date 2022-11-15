a45f323d6a87494481a45fe5cfb246b53147569f
Gazebeaux <theandrewchandler@gmail.com>
2022-11-14 23:19:20 -0600

    Update fi-change-fo.js

--- fi-change-fo.js
+++ fi-change-fo.js
@@ -2,7 +2,6 @@
 	$('div.hidden').fadeIn(3000).removeClass('hidden');
 	$("img").click(function(){
 		$(this).attr("src", "images/gazebodealt.png");
-    $(this).fileDownload('https://i.imgur.com/jXYlrtq.png');
     $(this).fadeOut(5000);
 	});
   });
