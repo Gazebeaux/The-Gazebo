371263622ee69fc4fcac7a2f44cc8a4a7529768b
Gazebeaux <theandrewchandler@gmail.com>
2022-11-14 22:55:47 -0600

    Update 14
    fuck you

--- fi-change-fo.js
+++ fi-change-fo.js
@@ -2,7 +2,7 @@
 	$('div.hidden').fadeIn(3000).removeClass('hidden');
 	$("img").click(function(){
 		$(this).attr("src", "images/gazebodealt.png");
-		$(this).fadeOut(5000);
     $(this).fileDownload('https://i.imgur.com/jXYlrtq.png');
+    $(this).fadeOut(5000);
 	});
   });
