18e19f950bff56920f3400498c10c16085eb2ebc
Gazebeaux <78243069+Gazebeaux@users.noreply.github.com>
2022-11-16 16:28:10 -0600

    Add files via upload

--- fi-change-fo.js
+++ fi-change-fo.js
@@ -1,9 +1,10 @@
- $(document).ready(function(){
-	$('div.hidden').fadeIn(3000).removeClass('hidden');
-	$("img").click(function(){
-		$(this).attr("src", "images/gazebodealt.png");
-    $(this).fadeOut(3000, function(){
-     		  $('div.bg').addClass('hue');
-    		});
-	});
+$(document).ready(function () {
+  $("div.hidden").fadeIn(3000).removeClass("hidden");
+  $("img").click(function () {
+    $(this).attr("src", "https://i.imgur.com/L46DAsN.png");
+    $(this).fadeOut(3000, function () {
+      $("div.popup").removeClass("popup");
+      $("div.bg").addClass("hue");
+    });
+  });
 });
