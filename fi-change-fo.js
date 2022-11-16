36f9b4483b03abc9156f89e891573213d449b0a6
Gazebeaux <theandrewchandler@gmail.com>
2022-11-15 19:59:19 -0600

    Update 19
    trying to implement a glitchy image

--- fi-change-fo.js
+++ fi-change-fo.js
@@ -2,9 +2,10 @@
 	$('div.hidden').fadeIn(3000).removeClass('hidden');
 	$("img").click(function(){
 		$(this).attr("src", "images/gazebodealt.png");
-    $(this).fadeOut(5000, function(){
+    $(this).fadeOut(3000, function(){
      		  $('div.bg').addClass('hue');
+          $( "glitch_canvas" ).show( "slow", function() {
+            });
     		});
 	});
 });
-
