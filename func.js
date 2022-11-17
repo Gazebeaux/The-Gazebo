9e07556ee78f66be9622d82ee8ac6e40941fa5c1
Gazebeaux <theandrewchandler@gmail.com>
2022-11-17 01:25:09 -0600

    26
    this is hard

--- func.js
+++ func.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $("div.hidden").fadeIn(3000).removeClass("hidden");
   $("img").click(function () {
-    $(this).attr("src", "https://i.imgur.com/L46DAsN.png");
+    $(this).attr("src", "images/gazebodealt.png");
     $(this).fadeOut(3000, function () {
       $("div.popup").removeClass("popup");
       $("div.bg").mousemove(function(e) {
