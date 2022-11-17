9b60bd8b4e3dce053b2500ada0e1393e20e22b4e
Andrew Chandler <78243069+Gazebeaux@users.noreply.github.com>
2022-11-17 10:25:12 -0600

    Update func.js
    hover instead of click

--- func.js
+++ func.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("div.hidden").fadeIn(3000).removeClass("hidden");
-  $("img").click(function () {
+  $("img").hover(function () {
     $(this).attr("src", "images/gazebodealt.png");
     $(this).fadeOut(3000, function play() {
            var audio = document.getElementById("audio");
