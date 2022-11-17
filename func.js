01287b880b71870d46fa1f2f822f9bf63132d1b6
Gazebeaux <theandrewchandler@gmail.com>
2022-11-17 03:28:17 -0600

    Update 27
    Got the  hue on mousemove right (separate from background)

--- func.js
+++ func.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
       $("div.bg").mousemove(function(e) {
         var X = e.pageX;
         var Y = e.pageY;
-        $("div.bg").css("filter", "hue-rotate(" + (.1 * X + Y * .1) + "deg)");
+        $("div.bdfilter").css("backdrop-filter", "hue-rotate(" + (.1 * X + Y * .1) + "deg)");
       });
   });
 });
