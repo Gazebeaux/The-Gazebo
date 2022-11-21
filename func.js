333817058bccfd9a73cef50b431b2ddefc6ab8a3
Gazebeaux <theandrewchandler@gmail.com>
2022-11-21 14:03:09 -0600

    35
    almost ready to show off to no one

--- func.js
+++ func.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
   $("div.hidden").fadeIn(1000).removeClass("hidden");
   $("img").click(function () {
     $(this).attr("src", "https://i.imgur.com/mFSlehC.png");
-   $(canvas).css("background-image", "url(" + imageUrl + ")");
-      $("div.bg").mousemove(function(e) {
+    $("div.canvas").css("background", "url(https://i.imgur.com/FyViU1P.png)");
+      $(document).mousemove(function(e) {
         var X = e.pageX;
         var Y = e.pageY;
         $("div.bdfilter").css("backdrop-filter", "hue-rotate(" + (.1 * X + Y * .1) + "deg)");
