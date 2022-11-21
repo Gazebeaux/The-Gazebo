50ee42f75965e8b2f7af76868fc03423b278c2bb
Gazebeaux <theandrewchandler@gmail.com>
2022-11-21 12:20:35 -0600

    33
    this is p close

--- func.js
+++ func.js
@@ -1,5 +1,13 @@
-$("div.bg").mousemove(function(e) {
+import $ from "https://cdn.skypack.dev/jquery@3.6.1";
+$(document).ready(function () {
+  $("div.hidden").fadeIn(1000).removeClass("hidden");
+  $("img").click(function () {
+    $(this).attr("src", "https://i.imgur.com/IBy5OZb.png");
+   $(canvas).css("background-image", "url(" + imageUrl + ")");
+      $("div.bg").mousemove(function(e) {
         var X = e.pageX;
         var Y = e.pageY;
         $("div.bdfilter").css("backdrop-filter", "hue-rotate(" + (.1 * X + Y * .1) + "deg)");
       });
+  });
+});
