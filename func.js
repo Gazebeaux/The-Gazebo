60136cc8ae873a3cbab028bbcade6170a4ad52ef
Gazebeaux <theandrewchandler@gmail.com>
2022-11-22 01:18:34 -0600

    46
    i usck

--- func.js
+++ func.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $("div.hidden").fadeIn(1500).removeClass("hidden");
   $("img").click(function () {
-    $(this).attr("src", "https://i.imgur.com/mFSlehC.png");
-    $("div.canvas").css("background", "url(https://i.imgur.com/FyViU1P.png)");
+    $(this).attr("src", "images/gazebodealt.png");
+    $("div.canvas").css("background", "url(images/pretty.png)");
       $(document).mousemove(function(e) {
         var X = e.pageX;
         var Y = e.pageY;
